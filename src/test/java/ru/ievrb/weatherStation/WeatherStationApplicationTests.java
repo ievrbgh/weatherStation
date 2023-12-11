747337690959fb73e5a60b0f6760d38da4646fc5
ievrbga <iev.rb.ga@gmail.com>
2023-12-11 13:53:49 +0300

    initialize project

---
+++ src/test/java/ru/ievrb/weatherStation/WeatherStationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ievrb.weatherStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
